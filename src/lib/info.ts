99d340e84911e843b4ded1e106639ca5aeced2db
ForrestDevs <luke.gannon@me.com>
2024-12-05 14:02:54 -0500

    feat: update twitter link

--- src/lib/info.ts
+++ src/lib/info.ts
@@ -2,7 +2,7 @@ export const info = {
   title: "Strawberry Coin",
   description: "The world's first and only strawberry-themed token",
   twitter: "@strawberrycoinsol",
-  twitterUrl: "https://twitter.com/strawberrycoinsol",
+  twitterUrl: "https://x.com/strawberrywagmi",
   telegram: "@strawberrycoinsol",
   telegramUrl: "https://t.me/strawberrycoinsol",
   contractAddress: "0x0000000000000000000000000000000000000000",
